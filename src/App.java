77f534520fc8301fe770e3fcbbd73cb20e3c85ea
Omar Hosny <honsyomar022@gmail.com>
2022-08-26 18:45:52 +0200

    Two Proplems Solved

--- src/App.java
+++ src/App.java
@@ -1,8 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        System.out.println(isPowerOfTwo(0));
+
     }
+    public static boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> HM = new HashMap<Integer,Integer>();  
+        for(int i = 0; i <arr.length; i++){
+            int currentEle = arr[i];
+            if(HM.get(currentEle) == null){
+                HM.put(currentEle, 1);
+            }else{
+                HM.put(currentEle, HM.get(currentEle) + 1);
+            }
+        }
+        HashSet<Integer> HS = new HashSet<Integer>(HM.values());
+            
+        return HS.size() == HM.values().size();
+    }
+
     public static  boolean isPowerOfTwo(int n){
         if(n == 1 || n == 2) return true;
         while(n > 0 && n % 2 == 0){
@@ -12,3 +31,4 @@ public class App {
         return false;
     } 
 }
+// [1,2,2,1,1,3]
